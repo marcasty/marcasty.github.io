dffbea661a7b16907b137e3684cffde90c1d3b8a
Mark Castellano <markacastellano2@gmail.com>
2025-06-08 13:35:10 -0400

    simplify build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {output: "export"}
-
-module.exports = nextConfig
+  /** @type {import('next').NextConfig} */
+  const nextConfig = {
+    output: "export",  // This tells Next.js to generate static files
+    basePath: process.env.NODE_ENV === 'production' ? '/marcasty.github.io' : '',  // Adjust if your site is not at the root
+    images: {
+      unoptimized: true  // Required for static export
+    }
+  }
+  module.exports = nextConfig
